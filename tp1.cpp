220d8b4b54a61cce87ced64439393e2336ccda85
Mihir Chauhan <mihir.hc@iitgn.ac.in>
2020-06-02 22:23:06 +0530

    Update tp1.cpp

--- tp1.cpp
+++ tp1.cpp
@@ -1,15 +1,32 @@
 #include<iostream>
-int n;
+#include<algorithm>
 using namespace std;
-int num(unsigned long long int a,unsigned long long int b, int i){
-    if(i==n){
-        return a;
-    }else{
-        return num(b,a+(b*b),i+1);
-    }
-}
+
 int main(){
-    unsigned long long int a,b;
-    cin>>a>>b>>n;
-    cout<<num(a,b,1);
+    int n;
+    cin>>n;
+    int arr[n];
+    for(int k=0; k<n; k++){
+        cin>>arr[k];
+    }
+    sort(arr,arr+n);
+    int score=0;
+    int i=0;
+    int max;
+    int min=arr[0];
+    while (arr[n]-arr[i]>4){
+        min=arr[i];
+        for(int j=i+1; j<n; j++ ){
+            max=arr[j];
+            if(max-min>4){
+                score+=1;
+                i=j;
+                break;
+            }
+        }
+    }
+
+    cout<<score+1;
+    
+
 }
